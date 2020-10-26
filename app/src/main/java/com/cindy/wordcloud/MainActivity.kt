053a389d10abe1797b88021f12a1a2f401edc62f
Cindy <cindychiu2008@gmail.com>
2020-10-26 17:25:27 +0800

    commit change
    commit change

--- app/src/main/java/com/cindy/wordcloud/MainActivity.kt
+++ app/src/main/java/com/cindy/wordcloud/MainActivity.kt
@@ -3,20 +3,22 @@ package com.cindy.wordcloud
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.ViewTreeObserver
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = javaClass.simpleName
-    private val mColorList: ArrayList<Int> = arrayListOf(
+    private val mRootXArray: MutableList<Int> = mutableListOf()
+    private val mRootYArray: MutableList<Int> = mutableListOf()
+    private val mWordCloudTextViewList: MutableList<TextView> = mutableListOf()
+    private val mColorList: MutableList<Int> = mutableListOf(
         android.R.color.background_dark,
-        android.R.color.background_light,
         android.R.color.holo_red_dark,
         android.R.color.holo_blue_dark,
         android.R.color.holo_green_dark,
@@ -24,6 +26,14 @@ class MainActivity : AppCompatActivity() {
         android.R.color.holo_orange_dark,
         android.R.color.holo_purple
     )
+    private val mWordCloudTextSizeList: MutableList<Float> = mutableListOf(
+        35f, 20f, 15f
+    )
+    private val mWordCloudTextList: MutableList<String> = mutableListOf(
+        "Text1", "Text2", "Text3", "Text4", "Text5", "Text6", "Text7", "Text8", "Text9", "Text10",
+        "Text11", "Text12", "Text13", "Text14", "Text15", "Text16", "Text17", "Text18", "Text19", "Text20"
+    )
+    private var mWordCloudTextViewDownCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,25 +51,50 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "width: ${vRoot.width}")
                 Log.w(TAG, "height: ${vRoot.height}")
                 vRoot.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                for (i in 0 until vRoot.width){
+                    mRootXArray.add(i)
+                }
+                for (i in 0 until vRoot.height){
+                    mRootYArray.add(i)
+                }
+                setTextView()
 
             }
         })
-        setFirstTextView()
 
     }
 
-    fun setFirstTextView(){
+    fun setTextView(x: Int = -1, y: Int = -1){
+        Log.v(TAG, "===== setTextView =====")
+        Log.w(TAG, "mWordCloudTextViewDownCount: $mWordCloudTextViewDownCount")
+        Log.w(TAG, "x: $x")
+        Log.w(TAG, "y: $y")
+
+        if(mWordCloudTextViewDownCount==mWordCloudTextList.size){
+            return
+        }
         val textView: TextView = TextView(this)
         val layoutParams: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT)
+        if(mWordCloudTextViewDownCount==0){
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT)
+        }else{
+            if(x!=-1){
+                layoutParams.leftMargin= x
+            }
+            if(y!=-1){
+                layoutParams.topMargin = y
+            }
+
+        }
         textView.layoutParams = layoutParams
-        textView.text = "Text1"
-        textView.textSize = 40f
-        val randomColor = getRandomColor()
-        Log.d(TAG, "mColorList: $mColorList")
-        Log.d(TAG, "randomColor: $randomColor")
-        textView.setTextColor(randomColor)
+        textView.id = R.id.word_cloud + mWordCloudTextViewDownCount
+        textView.text = mWordCloudTextList[mWordCloudTextViewDownCount]
+        textView.textSize = getRandomItemInList(mWordCloudTextSizeList) as Float
+        val randomColor = getRandomItemInList(mColorList)
+        textView.setBackgroundColor(ContextCompat.getColor(this, randomColor as Int))
+        textView.setTextColor(ContextCompat.getColor(this, android.R.color.white))
         vRoot.addView(textView)
+        mWordCloudTextViewDownCount++
         textView.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener{
             override fun onGlobalLayout() {
 
@@ -72,12 +107,109 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "width: ${textView.width}")
                 Log.i(TAG, "height: ${textView.height}")
                 textView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                if(!checkOverLapping(textView)){
+                    for (i in textView.left .. textView.right){
+                        if(mRootXArray.contains(i)){
+                            mRootXArray.remove(i)
+                        }
+                    }
+                    for (i in textView.top .. textView.bottom){
+                        if(mRootYArray.contains(i)){
+                            mRootYArray.remove(i)
+                        }
+                    }
+                    Log.i(TAG, "mRootXArray: $mRootXArray")
+                    Log.i(TAG, "mRootYArray: $mRootYArray")
+                    mWordCloudTextViewList.add(textView)
+                }else{
+                    vRoot.removeView(textView)
+                    mWordCloudTextViewDownCount--
+                }
+                if(mRootXArray.size>0 && mRootYArray.size>0){
+                    val nextX: Int = getRandomItemInList(mRootXArray) as Int
+                    val nextY: Int = getRandomItemInList(mRootYArray) as Int
+                    setTextView(nextX, nextY)
+                }
 
             }
         })
 
     }
 
-    fun getRandomColor() = mColorList[Random.nextInt(0, mColorList.size)]
+    fun getRandomItemInList(list: MutableList<*>) = list[Random.nextInt(0, list.size)]
 
+    fun checkOverLapping(textView: TextView): Boolean{
+        var isXOverLapping: Boolean = false
+        var isYOverLapping: Boolean = false
+        var overLapWidth: Int = 0
+        var overLapHeight: Int = 0
+
+        for(item in mWordCloudTextViewList){
+            Log.d(TAG, "X: ${item.x}")
+            Log.d(TAG, "Y: ${item.y}")
+            Log.d(TAG, "z: ${item.z}")
+            Log.d(TAG, "left: ${item.left}")
+            Log.d(TAG, "top: ${item.top}")
+            Log.d(TAG, "right: ${item.right}")
+            Log.d(TAG, "bottom: ${item.bottom}")
+            Log.d(TAG, "width: ${item.width}")
+            Log.d(TAG, "height: ${item.height}")
+
+            outSite@for(i in item.left .. item.right){
+                inSite@for(j in textView.left .. textView.right){
+                    if(i==j){
+                        isXOverLapping = true
+                        break@outSite
+                    }
+                }
+            }
+
+            outSite@for(i in item.top .. item.bottom){
+                inSite@for(j in textView.top .. textView.bottom){
+                    if(i==j){
+                        isYOverLapping = true
+                        break@outSite
+                    }
+                }
+            }
+
+            if(isXOverLapping && isYOverLapping){
+                Log.i(TAG, "重疊了，計算重覆面積")
+                overLapWidth = if(item.left <= textView.left){
+                    if(item.right <= textView.right){
+                        item.width - textView.left
+                    }else{
+                        textView.width
+                    }
+                }else{
+                    if(item.right <= textView.right){
+                        textView.right - item.left
+                    }else{
+                        item.width
+                    }
+                }
+
+                overLapHeight = if(item.top <= textView.top){
+                    if(item.bottom <= textView.bottom){
+                        item.height - textView.top
+                    }else{
+                        textView.height
+                    }
+                }else{
+                    if(item.bottom <= textView.bottom){
+                        item.height
+                    }else{
+                        textView.bottom - item.top
+                    }
+                }
+
+                Log.e(TAG, "overLapWidth: $overLapWidth")
+                Log.e(TAG, "overLapHeight: $overLapHeight")
+                if(overLapWidth * overLapHeight >= (item.width * item.height + textView.width * textView.height) / 3 * 2) return true
+
+            }
+
+        }
+        return false
+    }
 }
